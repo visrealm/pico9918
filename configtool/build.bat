853155f4e5b8bbe639aa202aa229b9033b02e090
Troy Schrapel <troy@schrapel.id.au>
2024-11-25 15:01:57 +1030

    configtool build refector

--- configtool/build.bat
+++ configtool/build.bat
@@ -8,25 +8,28 @@ mkdir build\bin 2> NUL
 pushd build
 
 set PATH=..\tools\cvbasic;%PATH%
+set LIBPATH=..\src\lib
+
 
 del *.bas
 copy /Y ..\src\*.bas .
+copy /Y ..\src\lib lib
 
 
 :: TI-99
-cvbasic --ti994a pico9918tool.bas asm\pico9918tool_ti99.a99 lib
+cvbasic --ti994a pico9918tool.bas asm\pico9918tool_ti99.a99 %LIBPATH%
 call assm asm/pico9918tool_ti99.a99
 if %errorlevel% neq 0 exit /b %errorlevel%
 linkticart.py pico9918tool_ti99_b00.bin bin\pico9918tool_ti99_8.bin "PICO9918 CONFIG TOOL"
 copy /Y bin\pico9918tool_ti99_8.bin c:\tools\Classic99
 
 :: ColecoVision
-cvbasic pico9918tool.bas asm\pico9918tool_cv.asm lib
+cvbasic pico9918tool.bas asm\pico9918tool_cv.asm %LIBPATH%
 gasm80 asm\pico9918tool_cv.asm -o bin\pico9918tool_cv.rom
 copy /Y bin\pico9918tool_cv.rom c:\tools\Classic99Phoenix
 
 :: MSX
-cvbasic --msx pico9918tool.bas asm\pico9918tool_msx.asm lib
+cvbasic --msx pico9918tool.bas asm\pico9918tool_msx.asm %LIBPATH%
 gasm80 asm\pico9918tool_msx.asm -o bin\pico9918tool_msx.rom
 
 
